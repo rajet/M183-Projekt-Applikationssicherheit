5ed6704a4832d03ffeb038bc84bf8929cd7b9e28
rajethan.ranjan <rajethan.ranjan@outlook.com>
2018-01-12 10:57:30 +0100

    Projekt wurde erstellt

---
+++ Projekt-Applikationssicherheit/Projekt-Applikationssicherheit/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Projekt_Applikationssicherheit
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
